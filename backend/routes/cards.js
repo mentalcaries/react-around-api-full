612d89a4ef48089e115611708de1cbda65709026
Devin Jaggernauth <neexis@gmail.com>
2021-12-17 08:47:32 -0400

    some change

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -20,7 +20,7 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().custom(validateUrl),
   }),
 }), createCard);
 
